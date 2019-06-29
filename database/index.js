4bc0fb26f90fee0cda8e5ad9504c079870979502
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-28 22:31:42 -0700

    pases repos data coming from server, iterates through and creates a new Repo instance for each

--- database/index.js
+++ database/index.js
@@ -1,26 +1,38 @@
 const mongoose = require('mongoose');
+//fetcher is the name of the database
 mongoose.connect('mongodb://localhost/fetcher');
 
-let repoSchema = mongoose.Schema({
+var db = mongoose.connection;
 
-  id: Number,
-  name:String,
-  owner: {
-    login: String,
-    url: String
-  },
-  html_url: String,
-  forks: Number
+let repoSchema = mongoose.Schema({
+  id: 'number',
+  name:'string',
+  html_url: 'string',
+  forks: 'number',
+    owner: {
+      login: 'string',
+      url: 'string'
+    }
 });
 
+// The first argument is the singular name of the collection your model is for. ** Mongoose automatically looks for the plural, lowercased version of your model name. ** Thus, for the example above, the model Tank is for the tanks collection in the database.
 let Repo = mongoose.model('Repo', repoSchema);
 
 let save = (repos) => {
-  //declare repo here
-  // iterate through repos
-  //for every repo extract values and add to repo schema
-  //use save() to save that model in the db
-
+  repos = JSON.parse(repos);
+  for (var i = 0; i < repos.length; i++) {
+    let repo = repos[i];
+    var doc = new Repo({
+      id: repo.id,
+      name:repo.name,
+      owner: {
+        login: repo.owner.login,
+        url: repo.owner.url
+      },
+      html_url: repo.html_url,
+      forks: repo.forks
+    });
+    doc.save((err) => console.log(err));
+  }
 }
-
-module.exports.save = save;
+module.exports = save;
