04a33f39bb4d0ea6b06d34431a4c899a180d91d5
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-28 19:24:32 -0700

    created schema for each repo and outline for the save method

--- database/index.js
+++ database/index.js
@@ -2,15 +2,25 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+
+  id: Number,
+  name:String,
+  owner: {
+    login: String,
+    url: String
+  },
+  html_url: String,
+  forks: Number
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
+let save = (repos) => {
+  //declare repo here
+  // iterate through repos
+  //for every repo extract values and add to repo schema
+  //use save() to save that model in the db
+
 }
 
 module.exports.save = save;
