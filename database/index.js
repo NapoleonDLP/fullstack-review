50142867e08999606dbe107e03ea619620525b93
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-29 01:02:48 -0700

    Using different method to write to DB

--- database/index.js
+++ database/index.js
@@ -1,30 +1,28 @@
 const mongoose = require('mongoose');
-//fetcher is the name of the database
 mongoose.connect('mongodb://localhost/fetcher');
 
-var db = mongoose.connection;
-
-let repoSchema = mongoose.Schema({
-  id: 'number',
-  name:'string',
-  html_url: 'string',
-  forks: 'number',
+let repoSchema = new mongoose.Schema({
+  id: Number,
+  name: String,
+  html_url: String,
+  forks: Number,
     owner: {
-      login: 'string',
-      url: 'string'
+      login: String,
+      url: String
     }
 });
 
 // The first argument is the singular name of the collection your model is for. ** Mongoose automatically looks for the plural, lowercased version of your model name. ** Thus, for the example above, the model Tank is for the tanks collection in the database.
-let Repo = mongoose.model('Repo', repoSchema);
+var Repo = mongoose.model('Repo', repoSchema);
 
-let save = (repos) => {
+let save = async (repos) => {
   repos = JSON.parse(repos);
   for (var i = 0; i < repos.length; i++) {
     let repo = repos[i];
+
     var doc = new Repo({
       id: repo.id,
-      name:repo.name,
+      name: repo.name,
       owner: {
         login: repo.owner.login,
         url: repo.owner.url
@@ -32,7 +30,14 @@ let save = (repos) => {
       html_url: repo.html_url,
       forks: repo.forks
     });
-    doc.save((err) => console.log(err));
+
+    Repo.findOneAndUpdate({id: doc.id}, doc, {upsert: true}, (err) => {
+      console.log('***********');
+      console.log(err);
+      console.log('***********');
+    });
+    // doc.save();
   }
 }
+
 module.exports = save;
