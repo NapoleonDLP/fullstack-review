7147406db1049616fae07a9800c9b2da8080b947
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-29 01:03:24 -0700

    cleaned up

--- helpers/github.js
+++ helpers/github.js
@@ -4,11 +4,8 @@ const request = require('request');
 const config = require('../config.js');
 const save = require('../database/index.js');
 
-// const bodyParser = require('body-parser');
-
 let getReposByUsername = (username) => {
-  // TODO - Use the request module to request repos for a specific
-  // user from the github API
+
   let options = {
     url: `https://api.github.com/users/${username}/repos`,
     method: 'GET',
@@ -18,14 +15,10 @@ let getReposByUsername = (username) => {
     }
   };
 
-  // The options object has been provided to help you out,
-  // but you'll have to fill in the URL
   request(options, (err, res, data) => {
     if (err) {
       console.log(err);
     } else {
-      //responseBody is an array of all that user info
-      // console.log(res.body);
       save(res.body);
     }
   })
