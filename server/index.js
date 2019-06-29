b76dd576bcff9466155e0a3b0e8e0afa64d4869f
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-28 17:16:14 -0700

    invoke the getReposByUsername

--- server/index.js
+++ server/index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 let app = express();
+const bodyParser = require('body-parser');
+const getReposByUsername = require('../helpers/github.js');
 
 app.use(express.static(__dirname + '/../client/dist'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.post('/repos', function (req, res) {
   // TODO - your code here!
   // This route should take the github username provided
   // and get the repo information from the github API, then
   // save the repo information in the database
+  getReposByUsername(req.body.username);
 });
 
 app.get('/repos', function (req, res) {
