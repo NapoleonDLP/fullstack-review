341e0e4d24d30633261ea700a9d7e13dc770fd1f
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-28 17:14:34 -0700

    fill in correct url, import request, use request to make GET to github api

--- helpers/github.js
+++ helpers/github.js
@@ -1,20 +1,34 @@
+const fs = require('fs');
+const tls = require('tls');
 const request = require('request');
 const config = require('../config.js');
+// const bodyParser = require('body-parser');
 
-let getReposByUsername = (/* TODO */) => {
+let getReposByUsername = (username) => {
   // TODO - Use the request module to request repos for a specific
   // user from the github API
-
-  // The options object has been provided to help you out, 
-  // but you'll have to fill in the URL
   let options = {
-    url: 'FILL ME IN',
+    url: `https://api.github.com/users/${username}/repos`,
+    method: 'GET',
     headers: {
       'User-Agent': 'request',
       'Authorization': `token ${config.TOKEN}`
     }
   };
 
-}
 
-module.exports.getReposByUsername = getReposByUsername;
+
+  // The options object has been provided to help you out,
+  // but you'll have to fill in the URL
+  request(options, (err, res, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      //responseBody is an array of all that user info
+      // console.log(res.body);
+      console.log(JSON.parse(res.body))
+    }
+  })
+};
+
+module.exports = getReposByUsername;
