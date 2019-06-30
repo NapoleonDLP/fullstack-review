6f2c6bb4d905511cfa28566f3ebb644a60f608f1
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-29 17:31:54 -0700

    renders top 25 repos sorted by most repos

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -1,10 +1,51 @@
 import React from 'react';
 
-const RepoList = (props) => (
-  <div>
-    <h4> Repo List Component </h4>
-    There are {props.repos.length} repos.
-  </div>
-)
+var RepoList = (props) => {
+// class RepoList extends React.Component {
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     repos: ['repo', 'repo']
+  //   }
+  //   this.loadRepos = this.loadRepos.bind(this);
+  // }
+
+  // ComponentWillMount () {
+  //   console.log('YUP THIS RAN')
+  //   // this.loadRepos();
+  // }
+
+  // loadRepos() {
+  //   console.log('loadRepos is called')
+  //   fetch('http://localhost:1128/repos')
+  //   .then(results => response.json())
+  //   .then(result => {
+  //     this.setState({respos: result})
+  //   })
+  // }
+
+  // ComponentDidMount () {
+  //   console.log('ANND FINALLY THIS RAN')
+  //   // this.loadRepos();
+  // }
+
+
+
+    return (
+      <div>
+        <h4> Repo List Component </h4>
+        There are {props.repos.length} repos.
+        {props.repos.map(repo =>
+          <div>
+            <div>Repo Name: {repo.name}</div>
+            <div><a herf={repo.html_url}>URL: {repo.html_url}</a></div>
+            <div>Forks: {repo.forks}</div>
+            <div>User: {repo.owner}</div>
+
+          </div>
+        )}
+      </div>
+    )
+}
 
 export default RepoList;
