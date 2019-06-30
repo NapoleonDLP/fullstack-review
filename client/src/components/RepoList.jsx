009a4963de44755feb10af96211c08cc4ae126fd
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-30 00:07:42 -0700

    turn into class component and manages its own state. Makes fetch to server for repoList

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -1,50 +1,36 @@
 import React from 'react';
 
-var RepoList = (props) => {
-// class RepoList extends React.Component {
-  // constructor(props) {
-  //   super(props)
-  //   this.state = {
-  //     repos: ['repo', 'repo']
-  //   }
-  //   this.loadRepos = this.loadRepos.bind(this);
-  // }
+class RepoList extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      repos: ['repo', 'repo']
+    }
+  }
 
-  // ComponentWillMount () {
-  //   console.log('YUP THIS RAN')
-  //   // this.loadRepos();
-  // }
+  componentDidMount () {
+    fetch('http://localhost:1128/repos')
+    .then(response => response.json())
+    .then(result => this.setState({repos: result}))
+    .catch(err => console.log(err));
+  }
 
-  // loadRepos() {
-  //   console.log('loadRepos is called')
-  //   fetch('http://localhost:1128/repos')
-  //   .then(results => response.json())
-  //   .then(result => {
-  //     this.setState({respos: result})
-  //   })
-  // }
-
-  // ComponentDidMount () {
-  //   console.log('ANND FINALLY THIS RAN')
-  //   // this.loadRepos();
-  // }
-
-
-
+  render () {
     return (
       <div>
         <h4> Repo List Component </h4>
-        There are {props.repos.length} repos.
-        {props.repos.map(repo =>
-          <div>
-            <li>Repo Name: {repo.name}</li>
-            <ul><a href={repo.html_url} target="_blank">URL: {repo.html_url}</a></ul>
-            <ul>Forks: {repo.forks}</ul>
-            <ul>User: {repo.owner}</ul>
-          </div>
-        )}
+          There are {this.state.repos.length} repos.
+            {this.state.repos.map(repo =>
+              <div>
+                <div>User: {repo.owner}</div>
+                <div>Repo name: {repo.name}</div>
+                <div>Forks: {repo.forks}</div>
+                <div><a href={repo.html_url} target="_blank">URL: {repo.html_url}</a></div>
+              </div>
+            )}
       </div>
     )
+  }
 }
 
 export default RepoList;
