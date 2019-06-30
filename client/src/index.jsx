d339a422e6126c8d05b0b9298be2f20b09fdc9da
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-30 00:06:45 -0700

    removes functionality for RepoList component to be handled in its own component including its own props

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -10,51 +10,23 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      repos: ['repo', 'repo']
     }
-  this.loadRepos = this.loadRepos.bind(this);
   }
 
-  loadRepos() {
-    fetch('http://localhost:1128/repos', {
-    headers: {
-      "Accept": 'text/plain',
-      "Content-Type": 'application/json'
-    }})
-    .then(response => response.json())
-    .then(json => console.log(json))
-    .then(result => this.setState({repos: result}))
-    .then(result => console.log('****',result))
-    // .catch(err => console.log(err));
-      // this.setState({repos: result})
-  }
-
-  componentDidMount () {
-    // this.loadRepos();
-    var data = fetch('http://localhost:1128/repos')
-    .then(response => response.json())
-    // .then(obj => obj.json())
-    .then(result => this.setState({repos: result}))
-    .then(result => console.log(result))
-    // .catch(err => console.log(err));
-      // this.setState({repos: result})
-  }
-
-  search (term) {
+  search (term, cb) {
     $.ajax({
       type: "POST",
       url: 'http://localhost:1128/repos',
-      data: {username:term}
-      // success: success,
-      // dataType: dataType
+      data: {username:term},
+      success: cb
     });
   }
 
   render () {
     return (<div>
       <h1>Github Fetcher</h1>
-      <Search onSearch={this.search.bind(this)}/>
-      <RepoList repos={this.state.repos}/>
+      <Search load={this.loadRepos} onSearch={this.search.bind(this)}/>
+      <RepoList />
     </div>)
   }
 }
