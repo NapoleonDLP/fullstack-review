c092f947fa916cf75e3c3c316478ac9af1963aad
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-29 17:33:37 -0700

    adjust schemma to be easier traversed

--- database/index.js
+++ database/index.js
@@ -1,21 +1,18 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
-let repoSchema = new mongoose.Schema({
+let repoSchema = mongoose.Schema({
   id: Number,
   name: String,
   html_url: String,
   forks: Number,
-    owner: {
-      login: String,
-      url: String
-    }
+  owner: String
 });
 
 // The first argument is the singular name of the collection your model is for. ** Mongoose automatically looks for the plural, lowercased version of your model name. ** Thus, for the example above, the model Tank is for the tanks collection in the database.
 var Repo = mongoose.model('Repo', repoSchema);
 
-let save = async (repos) => {
+let save = (repos) => {
   repos = JSON.parse(repos);
   for (var i = 0; i < repos.length; i++) {
     let repo = repos[i];
@@ -23,14 +20,11 @@ let save = async (repos) => {
     var doc = new Repo({
       id: repo.id,
       name: repo.name,
-      owner: {
-        login: repo.owner.login,
-        url: repo.owner.url
-      },
       html_url: repo.html_url,
-      forks: repo.forks
+      forks: repo.forks,
+      owner: repo.owner.login
     });
-
+    console.log(doc)
     Repo.findOneAndUpdate({id: doc.id}, doc, {upsert: true}, (err) => {
       console.log('***********');
       console.log(err);
@@ -39,5 +33,13 @@ let save = async (repos) => {
     // doc.save();
   }
 }
+//condtion in {key: value}
+let query = (cb) => {
+  Repo.find().sort({forks: -1}).limit(25)
+  // .then(results => cb())
+  .then(results => {
+    return cb(null, results)})
+  .catch(err => console.log(err));
+}
 
-module.exports = save;
+module.exports = {save, query}
